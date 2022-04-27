9d5b8bc8f308cc53f48d4f34e8e4b422f3064e58
Michael Dales <michael@digitalflapjack.com>
2022-04-27 11:26:34 +0100

    Make template loading more flexible
    * First we add --templates to let you specify a different location for
      for the templates.
    * Secondly we remove hard-coded filenames for specific templates.

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ const RevisionLogLimit = 5
 var (
 	// default values, can be overriden by flags
 	directory = "files"
+	templates = "templates"
 	address   = ":8080"
 	title     = "gopages"
 	basepath  = "/"
@@ -25,6 +26,7 @@ var (
 func main() {
 	// Define command line flags and parse them
 	flagDirectory := flag.String("dir", directory, "directory where the markdown files are stored")
+	flagTemplates := flag.String("templates", templates, "directory where the templates are stored")
 	flagAddress := flag.String("address", address, "address for the webserver to bind to, example: 0.0.0.0:8000")
 	flagTitle := flag.String("title", title, "title to display")
 	flagBasepath := flag.String("basepath", basepath, "base path, for web application proxy pass")
@@ -32,6 +34,7 @@ func main() {
 
 	// Update global variables to possibly overriden ones
 	directory = *flagDirectory
+	templates = *flagTemplates
 	address = *flagAddress
 	title = *flagTitle
 	basepath = *flagBasepath
@@ -39,6 +42,11 @@ func main() {
 	// Check if wiki data directory exists
 	if _, err := os.Stat(directory); err != nil {
 		log.Fatalf("WARNING: the specified directory (%q) does not exist!", directory)
+	}
+
+	err := loadTemplates(templates)
+	if err != nil {
+		log.Fatalf("Error loading templates from %s: %v", templates, err)
 	}
 
 	// Static files (js, css, etc)
